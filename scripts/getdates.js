1b53d2db3e9b287c6008f7b3559c2a20189b0245
white-ninja <foreverquantum.leap@gmail.com>
2026-01-13 21:41:01 +0100

    Redeploying...

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,6 +2,6 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
 
-// Get the last modified date of the document.
+// Get the last modified date of the document..
 document.getElementById("lastModified").textContent = document.lastModified;
 
