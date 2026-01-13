3f7e3170f97f49a2650f88e64169a6719e7f6527
white-ninja <foreverquantum.leap@gmail.com>
2026-01-13 17:39:12 +0100

    Design and develop a home page as my wdd131 course landing page. Used my own content and basic styling.

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Display the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+// Display the last modified date 
+document.getElementsById("lastModified").textContent = document.lastModified;
+
