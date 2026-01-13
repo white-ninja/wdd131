ed47f8a9b9c4cb8da790a063fb310eb2b6b805c5
white-ninja <foreverquantum.leap@gmail.com>
2026-01-13 19:54:59 +0100

    In index.html & In getdates.js, rewrote JavaScript statements that dynamically outputs the current year and last modified date and time.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,16 +1,7 @@
-// Display the current year
+// Get the current year
 const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
 
-// Display the last modified date 
-document.getElementsById("lastModified").textContent = document.lastModified;
+// Get the last modified date of the document
+document.getElementById("lastModified").textContent = document.lastModified;
 
-fetch('https://api.github.com/repos/white-ninja/wdd131/commits?per_page=1')
-  .then(response => response.json())
-  .then(data => {
-    const lastCommit = new Date(data[0].commit.committer.date);
-    document.getElementById("lastModified").textContent = lastCommit.toLocaleString();
-  })
-  .catch(err => {
-    console.error('Could not fetch commit info', err);
-  });
