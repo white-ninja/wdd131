66a1c0735aa0538144b5aa451a61c8add681e27d
white-ninja <foreverquantum.leap@gmail.com>
2026-01-13 19:16:04 +0100

    Added some JavaScript code to use GitHub API to get commit date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,12 @@ document.getElementById("year").textContent = currentYear;
 // Display the last modified date 
 document.getElementsById("lastModified").textContent = document.lastModified;
 
+fetch('https://api.github.com/repos/white-ninja/wdd131/commits?per_page=1')
+  .then(response => response.json())
+  .then(data => {
+    const lastCommit = new Date(data[0].commit.committer.date);
+    document.getElementById("lastModified").textContent = lastCommit.toLocaleString();
+  })
+  .catch(err => {
+    console.error('Could not fetch commit info', err);
+  });
